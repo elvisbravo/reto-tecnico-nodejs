97c387dc58a69cb77895509b48072e8ae797690d
70167122 <elvisbravo262@gmail.com>
2024-02-09 10:53:17 -0500

    reto tecnico aws test

--- test/serviceGetAllTasks.test.ts
+++ test/serviceGetAllTasks.test.ts
@@ -6,12 +6,16 @@ import Config from '../src/context/config';
 chai.use(chaiHttp);
 const expect = chai.expect;
 
+
 describe('serviceGetAllTasks', () => {
-  describe('serviceGetAllTasks', () => {
 
-    it('debería retornar los datos correctamente', async () => {
-      
-    });
-
+  it('debería retornar los datos correctamente', async () => {
+    const res = await chai
+      .request(Config.API_TASKS)
+      .get('/api/tasks');
+    expect(res).to.have.status(200);
+    expect(res.body.message).to.equal('Datos Cargados Correctamente');
+    expect(res.body.data).to.be.an('array');
   });
+
 });
